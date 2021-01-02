4d528456607297748e288d8837322ad0bc65528c
杉山素直 <sugiyamasunao@sugiyamasunaonoMacBook-Air.local>
2021-01-03 04:32:48 +0900

    first commit

--- codes/dog.py
+++ codes/dog.py
@@ -1,5 +1,16 @@
 import numpy as np
 class dog_class:
+    """
+    This is dog class
+
+    Attributes
+    ----------
+    name : str
+        name of dog
+    belongings : list
+        list of belongings
+
+    """
     def __init__(self, name='wanwan'):
         """
         init function of dog class
@@ -8,12 +19,15 @@ class dog_class:
         ----------
         name : str, optional
             name of dog, by default 'wanwan'
+
         """
         self.name = name
+        self.belongings = []
 
     def print_name(self):
         """
         printing name of dog
+
         """
         print(self.name)
 
@@ -25,5 +39,17 @@ class dog_class:
         -------
         name : str
             name of dog
+
         """
         return self.name
+
+    def give_present(self, present):
+        """
+        give present to dog
+
+        Parameters
+        ----------
+        present : obj
+            anything to give to dog
+        """
+        self.belongings.append(present)
